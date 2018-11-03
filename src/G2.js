94e3a9d105c5d96e2056a31fc7a6f3cf92934f92
hqiwen <1726564970@qq.com>
2018-11-03 14:56:45 +0800

    add monthData for G2

--- src/G2.js
+++ src/G2.js
@@ -1,25 +1,33 @@
-require(['data'], function (data) {
-    let data = data.getMonthWeekForG2();
-    function createG2Instance(mountId,width,height){
+define(['data'], function (data) {
+    var monthData = data.getMonthDataForG2();
+    function createG2Instance(mountId,width,height) {
         const chart = new G2.Chart({
             container: mountId, // 指定图表容器 ID
             width: width, // 指定图表宽度
             height: height
         });
-        chart.source(data, {
+        console.log(monthData);
+        chart.source(monthData, {
             weekDay: {
-                type: 'cat',
+                type: "cat",
                 values: ["日", "一", "二", "三", "四", "五", "六"]
             },
-            date: {
-                type: 'cat'
-            },
             weekIndex: {
-                type: 'cat',
-                values: ['5', '4', '3', '2', '1', '0']
+                type: "cat",
+                values: ["0", "1", "2", "3", "4", "5", "6"]
             }
-        })
-        chart.polygon().position('weekday*weekIndex').color('date');
+        });
+        //TODO add text for chart
+        chart
+            .polygon()
+            .position("weekDay*weekIndex")
+            .label("date", {
+                offset: 0,
+                textStyle: { fontSize: 14 }
+            }).style({
+                lineWidth: 1,
+                stroke: '#fff'
+            });
         chart.render();
     }
     return {
--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
-define(function () {
+define(function() {
     function getMonthData(year, month) {
         var ret = [];
 
         if (!year || !month) {
-          var today = new Date();
-          year = today.getFullYear();
-          month = today.getMonth() + 1;
+            var today = new Date();
+            year = today.getFullYear();
+            month = today.getMonth() + 1;
         }
 
         var firstDay = new Date(year, month - 1, 1);
@@ -23,28 +23,75 @@ define(function () {
 
         var lastDay = new Date(year, month, 0);
         var lastDate = lastDay.getDate();
-
+      //data = [{ weekDay: Number, weekIndex: Number, date: Number }]
         for (var i = 0; i < 7 * 6; i++) {
-          var date = i + 1 - preMonthDayCount;
-          var showDate = date;
-          var thisMonth = month;
-          if (date <= 0) {
-            thisMonth = month - 1;
-            showDate = lastDateOfLastMonth + date;
-          } else if (date > lastDate) {
-            thisMonth = month + 1;
-            showDate = showDate - lastDate;
-          }
-
-          if (thisMonth === 0) thisMonth = 12;
-          if (thisMonth === 13) thisMonth = 1;
-
-          ret.push({ month: thisMonth, date: date, showDate: showDate });
+            var date = i + 1 - preMonthDayCount;
+            var showDate = date;
+            var thisMonth = month;
+            if (date <= 0) {
+                thisMonth = month - 1;
+                showDate = lastDateOfLastMonth + date;
+            } else if (date > lastDate) {
+                thisMonth = month + 1;
+                showDate = showDate - lastDate;
+            }
+          
+            if (thisMonth === 0) thisMonth = 12;
+            if (thisMonth === 13) thisMonth = 1;
+          
+            ret.push({ month: thisMonth, date: date, showDate: showDate });
         }
         return { year: year, month: month, days: ret };
-      }
-
-    return {
-        getMonthData:getMonthData
     }
-})
+  function getMonthDataForG2(year, month) {
+    var ret = [];
+
+    if (!year || !month) {
+      var today = new Date();
+      year = today.getFullYear();
+      month = today.getMonth() + 1;
+    }
+    var firstDay = new Date(year, month - 1, 1);
+    var firstDayWeekDay = firstDay.getDay();
+    if (firstDayWeekDay === 0) firstDayWeekDay = 7;
+
+    year = firstDay.getFullYear();
+    month = firstDay.getMonth() + 1;
+
+    var lastDayOfLastMonth = new Date(year, month - 1, 0);
+    var lastDateOfLastMonth = lastDayOfLastMonth.getDate();
+
+    var preMonthDayCount = firstDayWeekDay - 1;
+
+    var lastDay = new Date(year, month, 0);
+    var lastDate = lastDay.getDate();
+
+    for (var i = 0; i < 7 * 6; i++) {
+      var date = i + 1 - preMonthDayCount;
+      var showDate = date;
+      var thisMonth = month;
+
+      if (date <= 0) {
+        thisMonth = month - 1;
+        showDate = lastDateOfLastMonth + date;
+      } else if (date > lastDate) {
+        thisMonth = month + 1;
+        showDate = showDate - lastDate;
+      }
+      if (thisMonth === 0) thisMonth = 12;
+      if (thisMonth === 13) thisMonth = 1;
+      var addDate = new Date(year, month - 1, date);
+      var intervalDays = Math.round((addDate.getTime() - firstDay.getTime()) / 86400000);
+      var weekDay = (intervalDays + firstDay.getDay()) % 7;
+      var weekIndex = Math.floor((intervalDays + firstDay.getDay()) / 7);
+
+      ret.push({
+        weekDay: weekDay,
+        weekIndex: weekIndex,
+        date: showDate
+      });
+    }
+    return ret;
+  }
+  return { getMonthData: getMonthData, getMonthDataForG2: getMonthDataForG2 };
+});
