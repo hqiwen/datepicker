cb06c69dab17fa564e5f800b30e90434997cf612
hqiwen <1726564970@qq.com>
2018-11-06 23:00:12 +0800

    add  funtion change month

--- src/G2.js
+++ src/G2.js
@@ -1,5 +1,9 @@
 define(['data'], function (data) {
-    var monthData = data.getMonthDataForG2();
+    var getMonthDataForG2 = data.getMonthDataForG2;
+    var monthData = getMonthDataForG2(2018, 0).days;
+    var year = getMonthDataForG2().year;
+    var month = getMonthDataForG2().month;
+    console.log(month);
     function createG2Instance(mountId,width,height) {
         const chart = new G2.Chart({
             container: mountId, // 指定图表容器 ID
@@ -46,7 +50,53 @@ define(['data'], function (data) {
                 }
             })
             .style({ lineWidth: 1, stroke: "gray" });
+        const nextMonthPoint = [4.5, -2.2]
+        const prevMonthPoint = [0.5, -2.2]
+        chart.guide().text({
+            position: prevMonthPoint,
+            content: "上个月",
+            style: {
+                fill: '#666', // 文本颜色
+                fontSize: '12', // 文本大小
+                fontWeight: 'bold' // 文本粗细
+            },
+            alignX: "left",
+            alignY: "bottom",
+            offsetX: 10,
+            appendInfo: {
+                id: 'prevMonth',
+            }
+        });
+        chart.guide().text({
+            position: nextMonthPoint,
+            content: "下个月",
+            style: {
+                fill: '#666', // 文本颜色
+                fontSize: '12', // 文本大小
+                fontWeight: 'bold' // 文本粗细
+            },
+            alignX: "right",
+            alignY: "bottom",
+            offsetX: 10,
+            appendInfo: {
+                id: 'nextMonth'
+            }
+        });
         chart.render();
+        chart.on('guide-text:click', ev => {
+            if (ev.appendInfo.id == "prevMonth") { 
+                month = month - 1;
+                monthData = getMonthDataForG2(year, month).days;
+                console.log(monthData);
+                chart.changeData(monthData);
+            } else if (ev.appendInfo.id == "nextMonth") { 
+                month = month + 1;
+                monthData = getMonthDataForG2(year, month).days;
+                console.log(monthData);
+                chart.changeData(monthData);
+            }
+            console.log('guide-text:click', ev.appendInfo);
+        });
     }
     return {
         createG2Instance: createG2Instance
